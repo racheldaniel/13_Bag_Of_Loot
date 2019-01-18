f758c7abaabe9948e86be2572fc329d23498a282
Rachel Daniel <rachdaniel88@gmail.com>
2019-01-18 09:42:47 -0600

    can now add a child and toy from cli

--- lootbag.py
+++ lootbag.py
@@ -8,7 +8,7 @@ python lootbag.py ls    -- lists children receiving presents
 python lootbag.py ls {child_name}   -- lists toys in bag for specific child
 python lootbag.py delivered {child_name} -- specify when toys are delivered
 """
-
+import sqlite3
 import sys
 for x in sys.argv:
      print("Argument: ", x)
@@ -16,8 +16,86 @@ for x in sys.argv:
 
 class Bag():
 
-    def __init__(self):
-        self.bag = []
+    __lootbag = '/Users/rachel/workspace/python/exercises/13_bag_of_loot/lootbag.db'
 
-    def add_toy(self):
-        for self.bag
+
+    def get_child(self, child_name):
+        with sqlite3.connect(self.__lootbag) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    f"SELECT * FROM Child WHERE Child.Name = '{child_name}'")
+                child = cursor.fetchall()
+                return child
+
+    def get_toy(self, toy_name):
+        with sqlite3.connect(self.__lootbag) as conn:
+                cursor = conn.cursor()
+
+                cursor.execute(f"SELECT * FROM Toy WHERE Name = '{toy_name}'")
+                toy = cursor.fetchall()
+                return toy
+
+    def call_funct(self, args):
+        """ Call the correct function based on sys.args
+            Takes one argument -- set of sys.args"""
+        if args[1] == "add" and len(args) == 4:
+            self.add_toy(args[2], args[3])
+
+    def add_toy(self, child_name, toy_name):
+        """ Add toy to bag-- first check to see if child exists. If not, add both a child and toy to the db
+
+            Takes two arguments:
+
+            child_name -- String
+            toy_name -- String
+        """
+        child_id = ""
+        child = self.get_child(child_name)
+        print(child)
+        if len(child) == 1:
+            child_id = child[0][0]
+            with sqlite3.connect(self.__lootbag) as conn:
+                cursor = conn.cursor()
+                try:
+                    cursor.execute(
+                        '''
+                        INSERT INTO Toy
+                        VALUES(?,?,?)
+                        ''', (None, toy_name, child_id)
+                    )
+                except sqlite3.OperationalError as err:
+                    print("oops", err)
+        else:
+            with sqlite3.connect(self.__lootbag) as conn:
+                cursor = conn.cursor()
+                try:
+                    cursor.execute(
+                        '''
+                        INSERT INTO Child
+                        VALUES(?,?,?)
+                        ''', (None, child_name, False)
+                    )
+                    child_id = cursor.lastrowid
+                except sqlite3.OperationalError as err:
+                    print("oops", err)
+
+            with sqlite3.connect(self.__lootbag) as conn:
+                cursor = conn.cursor()
+                try:
+                    cursor.execute(
+                        '''
+                        INSERT INTO Toy
+                        VALUES(?,?,?)
+                        ''', (None, toy_name, child_id)
+                    )
+                except sqlite3.OperationalError as err:
+                    print("oops", err)
+
+
+
+
+if __name__ == '__main__':
+    bag = Bag()
+    bag.call_funct(sys.argv)
+    print(bag.__dict__)
+
